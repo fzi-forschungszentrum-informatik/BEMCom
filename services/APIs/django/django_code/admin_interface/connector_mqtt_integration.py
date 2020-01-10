ee4f31503c60e71fda9553070c981d9a9eca44bb
hoeck <hoeck@fzi.de>
2020-01-10 12:23:53 +0100

    devl stuff removed

--- services/APIs/django/django_code/admin_interface/connector_mqtt_integration.py
+++ services/APIs/django/django_code/admin_interface/connector_mqtt_integration.py
@@ -161,7 +161,6 @@ class ConnectorMQTTIntegration():
         if message_type == 'mqtt_topic_available_datapoints':
             # TODO: Check how many of those entries exist already.
             for datapoint_type in payload:
-                #weird_value_string = ""
 
                 for key, example in payload[datapoint_type].items():
                     # Check if this available datapoint already exists in database
@@ -181,24 +180,16 @@ class ConnectorMQTTIntegration():
                                 'Exception while writing available datapoint into '
                                 'DB.'
                             )
-                    # TODO: remove condition after string has been fixed
-                    # TODO: create with with correct device and unit
-                    if True: #key.startswith("soap"):
-                        if not models.Datapoint.objects.filter(datapoint_key_in_connector=key).exists():
-                            # print("Get device")
-                            # device = models.Device.objects.filter(device_name="heatcontrol")[0]
-                            # print(device.device_name)
-                            try:
-                                _ = models.Datapoint(
-                                    datapoint_key_in_connector=key,
-                                ).save()
-                            except Exception:
-                                logger.exception(
-                                    'Exception while writing datapoint into DB.'
-                                )
-                    else:
-                        pass #weird_value_string += example
-                #print(weird_value_string)
+                    # TODO: only test version below -> create with correct device and unit
+                    if not models.Datapoint.objects.filter(datapoint_key_in_connector=key).exists():
+                        try:
+                            _ = models.Datapoint(
+                                datapoint_key_in_connector=key,
+                            ).save()
+                        except Exception:
+                            logger.exception(
+                                'Exception while writing datapoint into DB.'
+                            )
 
         if message_type == 'mqtt_topic_datapoint_map':
             for datapoint_type in payload:
