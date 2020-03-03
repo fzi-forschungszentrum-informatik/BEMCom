68e8f897166b49f91de4748b20a4e4253ff8511e
David Wölfle <woelfle@fzi.de>
2020-03-03 17:01:33 +0100

    Only display active datapoints

--- services/APIs/django/django_code/api/views.py
+++ services/APIs/django/django_code/api/views.py
@@ -5,5 +5,5 @@ from admin_interface.models.datapoint import Datapoint
 
 
 class DatapointViewSet(viewsets.ModelViewSet):
-    queryset = Datapoint.objects.all()
+    queryset = Datapoint.objects.filter(is_active=True)
     serializer_class = DatapointSerializer
