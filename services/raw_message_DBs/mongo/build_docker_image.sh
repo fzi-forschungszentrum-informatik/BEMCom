596fdb7b5164c9596f46098cec61edd2d1efeb7f
David Wölfle <woelfle@fzi.de>
2020-04-20 22:07:54 +0200

    Should be fully functional now. Container shutdown stops everything gracefully now.

--- services/raw_message_DBs/mongo/build_docker_image.sh
+++ services/raw_message_DBs/mongo/build_docker_image.sh
@@ -1,1 +1,1 @@
-docker build ./source -t bemcom/mongo-raw-message-db:0.0.1
+docker build ./source -t bemcom/mongo-raw-message-db:0.1.0
--- services/raw_message_DBs/mongo/source/entrypoint.sh
+++ services/raw_message_DBs/mongo/source/entrypoint.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-# TODO: The kill commands are not executed, it seems as the variable with the pid is empty.
-
-# Patches SIGTERM and SIGINT to stop background processes.
-# This is required for processes to gracefully shutdown if docker
-# wants to stop the container.
-trap "kill -TERM $mongo_pid" SIGTERM
-trap "kill -TERM $mqtt_integration_pid" SIGTERM
-#trap "kill -INT $mongo_pid" INT
-#trap "kill -INT $mqtt_integration_pid" INT
-
+# Start the DB in background.
 mongod &
 mongo_pid=$!
 
-# Boot the DB before connecting with MQTT.
+# Give the DB time to boot before connecting with MQTT.
 sleep 3
 
+# Connect the DB to MQTT.
 python3 /bemcom/mqtt_integration.py &
 mqtt_integration_pid=$!
 
+# Patch SIGTERM and SIGINT to the child processes, to allow them to shutdown 
+# gracefully if the container shuts down. Shutdown MQTT integration first so it
+# can finish a potential write process before we unplug the DB.
+trap "kill -INT $mqtt_integration_pid && sleep 1 && kill -INT $mongo_pid" SIGINT
+trap "kill -TERM $mqtt_integration_pid && sleep 1 && kill -TERM $mongo_pid" SIGTERM
 
-# Run processes until the container is stopped, also give proceses some time after receiving
-# SIGTERM to gracefully.
-wait
-sleep 2
+# Wait for the childprocesses to terminate, that will should happen only on error
+# or if the container is shut down.
+wait $mqtt_integration_pid
+wait $mongo_pid
