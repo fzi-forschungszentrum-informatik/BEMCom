eea7e1921788347b92a082210bc040c1b0d0d3d2
David Wölfle <woelfle@fzi.de>
2020-04-20 23:20:48 +0200

    Improve build script for all services.
    Builds now services templates first and does not show build output for successful builds

--- build_all_services.sh
+++ build_all_services.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 set -e
 
-# Finds the absolute bath of the build scripts
-find . -name build_docker_image.sh -exec readlink -f {} \;| while read build_script
+# Finds the absolute bath of the build scripts. Starts with the service_templates
+# first as these docker images might be required to build the services.
+for dir in service_templates services
 do
-    # And executes the build scripts in the correct directory to make local paths work.
-    script_dir=$(echo "$build_script" | rev | cut -d / -f2-1000 | rev)
-    cd "$script_dir"
-    bash build_docker_image.sh
+    find $dir -name build_docker_image.sh -exec readlink -f {} \;| while read build_script
+    do
+        # And executes the build scripts in the correct directory to make local paths work.
+        script_dir=$(echo "$build_script" | rev | cut -d / -f2-1000 | rev)
+        cd "$script_dir"
+        printf "Running $build_script"
+        # Preserve log output so we can only display output that failed.
+        {
+            build_out=$(bash build_docker_image.sh 2>&1) &&
+            echo -e "\e[1;32m OK \e[0m"
+        } || {
+            # This is only executed on error. Exit the script after printing what failed.
+            echo -e "\e[1;31m Failed! \e[0m"
+            printf "\nLog was:\n\n$build_out\n\n"
+            exit 1    
+        }
+    done
 done
