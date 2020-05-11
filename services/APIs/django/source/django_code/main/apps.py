5a2793783ca99083e387c12bbb4c1f42f049c399
David Wölfle <woelfle@fzi.de>
2020-05-11 15:27:46 +0200

    Also run MQTT integration in prod mode and clarify error message if MQTT broker is unreachable.

--- services/APIs/django/source/django_code/main/apps.py
+++ services/APIs/django/source/django_code/main/apps.py
@@ -1,5 +1,11 @@
 import sys
+import logging
+
 from django.apps import AppConfig
+
+
+logger = logging.getLogger(__name__)
+
 
 class MainConfig(AppConfig):
     name = 'main'
@@ -7,7 +13,17 @@ class MainConfig(AppConfig):
     def ready(self):
         import main.signals
 
-        if 'runserver' in sys.argv:
-            # Startup the MQTT integration.
+        if 'runserver' in sys.argv or "daphne" in sys.argv[0]:
+            logger.info("Starting up the main module of the API & Admin UI.")
             from .connector_mqtt_integration import ConnectorMQTTIntegration
-            ConnectorMQTTIntegration()
+            try:
+                ConnectorMQTTIntegration()
+            except ConnectionRefusedError:
+                logger.critical("")
+                logger.critical(
+                    "Could not connect to MQTT broker of backend. Exiting"
+                )
+                logger.critical("")
+                sys.exit(1)
+                
+            
