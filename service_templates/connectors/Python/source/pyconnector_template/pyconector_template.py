c4f4ad3f6115d4c74d3dd278cc506ae99e946d62
David Wölfle <woelfle@fzi.de>
2020-11-30 16:40:41 +0100

    WIP on pyconnector: Tests and implementation for SensorFlow.run.

--- service_templates/connectors/Python/source/pyconnector_template/pyconector_template.py
+++ service_templates/connectors/Python/source/pyconnector_template/pyconector_template.py
@@ -27,8 +27,6 @@ class SensorFlow():
         TODO
     """
 
-
-
     def run_sensor_flow(self, raw_data=None):
         """
         Processes data received from a device/gateway.
@@ -49,9 +47,38 @@ class SensorFlow():
         # Add receival timestamp in milliseconds since epoch.
         # (Following the message format.)
         ts_utc_now = round(datetime.timestamp(datetime.utcnow()) * 1000)
-        msg["timestamp"] = ts_utc_now
+        msg["payload"]["timestamp"] = ts_utc_now
 
-        self.mqtt_client.publish(payload=json.dumps(msg))
+        # Send raw msg to raw message DB if activated in settings.
+        # Handle bytes, as these are not JSON serializable.
+        if self.SEND_RAW_MESSAGE_TO_DB:
+            payload = msg["payload"]
+            if isinstance(payload["raw_message"], (bytes, bytearray)):
+                payload["raw_message"] = {
+                    "bytes": payload["raw_message"].decode()
+                }
+
+            topic = self.MQTT_TOPIC_RAW_MESSAGE_TO_DB
+            self.mqtt_client.publish(
+                payload=json.dumps(payload),
+                topic=topic,
+                qos=2,  # Ensures the message is received by the raw msg DB.
+            )
+
+        # Parse raw content to dict of dict.
+        msg = self.parse_raw_msg(self, raw_msg=msg)
+
+        # Flatten the parsed message to a single level dict.
+        msg = self.flatten_parsed_msg(self, parsed_msg=msg)
+
+        # Check if we found new datapoints in this message and need to
+        # send an update to the AdminUI.
+        self.update_available_datapoints(flattened_msg=msg)
+
+        # Publish values of datapoints that have been selected for such
+        # within the AdminUI.
+        self.filter_and_publish_datapoint_values(flattened_msg=msg)
+
 
     def receive_raw_msg(self, raw_data=None):
         """
@@ -73,10 +100,63 @@ class SensorFlow():
         msg : dict
             The message object containing the raw unprocessed data.
             Should be formated like this:
-            msg = {
-                "payload": {
-                    "raw_message": <the raw data>
+                msg = {
+                    "payload": {
+                        "raw_message": <the raw data>
+                    }
                 }
-            }
+            E.g.
+                msg = {
+                    "payload": {
+                        "raw_message": "device_1:{sensor_1:2.12,sensor_2:3.12}"
+                    }
+                }
         """
         raise NotImplementedError("receive_raw_msg has not been implemented.")
+
+    def parse_raw_msg(self, raw_msg):
+        """
+        Functionality to receive a raw message from device.
+
+        Poll the device/gateway for data and transforms this raw data
+        into the format epxected by run_sensor_flow. If the device/gateway
+        uses some protocol that pushes data, the raw data should be passed
+        as the raw_data argument to the function.
+
+        Parameters
+        ----------
+        raw_msg : dict.
+            Raw msg with data from device/gateway. Should be formated like:
+                msg = {
+                    "payload": {
+                        "raw_message": <the raw data>,
+                        "timestamp": <milliseconds since epoch>
+                    }
+                }
+
+        Returns
+        -------
+        msg : dict
+            The message object containing the parsed data as python dicts from
+            dicts strucuture.
+            Should be formated like this:
+                msg = {
+                    "payload": {
+                        "parsed_message": <the parsed data as object>,
+                        "timestamp": <milliseconds since epoch>
+                    }
+                }
+            E.g:
+                msg = {
+                    "payload": {
+                        "parsed_message": {
+                            "device_1": {
+                                "sensor_1": "2.12",
+                                "sensor_2": "3.12"
+                            }
+                        },
+                        "timestamp": 1573680749000
+                    }
+                }
+        """
+        raise NotImplementedError("parse_raw_msg has not been implemented.")
--- service_templates/connectors/Python/source/tests/test_pyconnector_template.py
+++ service_templates/connectors/Python/source/tests/test_pyconnector_template.py
@@ -21,16 +21,27 @@ class TestSensorFlowRun(TestClassWithFixtures):
         self.sf = SensorFlow()
 
         # Patch receive_raw_msg and add return value with realistic signature.
-        raw_msg_return = {
+        self.raw_msg_return = {
             "payload": {
-                "raw_message": "Some raw bytes or somtehing"
+                "raw_message": '{"dp_1": 2.1, "dp_2": "ok"}',
             }
         }
         self.sf.receive_raw_msg = MagicMock(
-            return_value=raw_msg_return
+            return_value=self.raw_msg_return
         )
 
+        # There is no control flow after the check for raw message DB that
+        # would depend on the content of the message. Just use these
+        # to check if the mehtods are called.
+        self.sf.parse_raw_msg = MagicMock()
+        self.sf.flatten_parsed_msg = MagicMock()
+        self.sf.update_available_datapoints = MagicMock()
+        self.sf.filter_and_publish_datapoint_values = MagicMock()
+
+        # Overload configuration that would be provided by the Connector.
         self.sf.mqtt_client = MagicMock()
+        self.sf.SEND_RAW_MESSAGE_TO_DB = False
+        self.sf.MQTT_TOPIC_RAW_MESSAGE_TO_DB = "tpyco/raw_message_to_db"
 
     def test_receive_raw_msg_is_called(self):
         """
@@ -45,10 +56,9 @@ class TestSensorFlowRun(TestClassWithFixtures):
         """
         self.sf.run_sensor_flow()
 
-        sent_payload = self.sf.mqtt_client.publish.call_args.kwargs["payload"]
-        sent_payload = json.loads(sent_payload)
+        msg = self.sf.parse_raw_msg.call_args.kwargs["raw_msg"]
 
-        assert "timestamp" in sent_payload
+        assert "timestamp" in msg["payload"]
 
     def test_timestamp_correct_value(self):
         """
@@ -63,10 +73,101 @@ class TestSensorFlowRun(TestClassWithFixtures):
 
         self.sf.run_sensor_flow()
 
-        sent_payload = self.sf.mqtt_client.publish.call_args.kwargs["payload"]
-        sent_payload = json.loads(sent_payload)
-        actual_ts = sent_payload["timestamp"]
+        msg = self.sf.parse_raw_msg.call_args.kwargs["raw_msg"]
+        actual_ts = msg["payload"]["timestamp"]
 
         assert actual_ts >= expected_ts
         assert actual_ts < expected_ts + 10000
 
+    def test_no_send_raw_msg_to_db_on_false(self):
+        """
+        Validate that no raw message is sent to the raw message DB if this
+        option is deactivated via setting flag.
+        """
+        self.sf.SEND_RAW_MESSAGE_TO_DB = False
+
+        self.sf.run_sensor_flow()
+
+        # publish should only been called once, i.e. for sending
+        # final message.
+        assert self.sf.mqtt_client.publish.call_count == 0
+
+    def test_send_raw_msg_to_db(self):
+        """
+        Check that the raw message is sent to raw message db if this option
+        is set.
+        """
+        self.sf.SEND_RAW_MESSAGE_TO_DB = True
+
+        self.sf.run_sensor_flow()
+
+        expected_raw_msg = self.raw_msg_return["payload"]["raw_message"]
+        expected_topic = self.sf.MQTT_TOPIC_RAW_MESSAGE_TO_DB
+        # Ensure the message is received by the raw message DB.
+        expected_qos = 2
+
+        publish_call = self.sf.mqtt_client.publish.call_args_list[0]
+        actual_topic = publish_call.kwargs["topic"]
+        actual_payload = json.loads(publish_call.kwargs["payload"])
+        actual_raw_msg = actual_payload["raw_message"]
+        actual_qos = publish_call.kwargs["qos"]
+
+        assert expected_raw_msg == actual_raw_msg
+        assert expected_topic == actual_topic
+        assert expected_qos == actual_qos
+
+    def test_send_raw_msg_to_db_bytes(self):
+        """
+        Check that the raw message is sent to raw message db if this option
+        is set. Here check special handling if raw message is in bytes,
+        as bytes cannot be serialized to JSON.
+        """
+        self.sf.SEND_RAW_MESSAGE_TO_DB = True
+        raw_msg_bytes_return = {
+            "payload": {
+                "raw_message": b'some bytes and stuff'
+            }
+        }
+        self.sf.receive_raw_msg = MagicMock(
+            return_value=raw_msg_bytes_return
+        )
+
+        self.sf.run_sensor_flow()
+
+        expected_raw_msg = {
+            "bytes": b'some bytes and stuff'.decode()
+        }
+
+        publish_call = self.sf.mqtt_client.publish.call_args_list[0]
+        actual_payload = json.loads(publish_call.kwargs["payload"])
+        actual_raw_msg = actual_payload["raw_message"]
+
+        assert expected_raw_msg == actual_raw_msg
+
+    def test_parse_raw_msg_called(self):
+        """
+        This function is an essential part of the run logic.
+        """
+        self.sf.run_sensor_flow()
+        self.sf.parse_raw_msg.assert_called()
+
+    def test_flatten_parsed_msg_called(self):
+        """
+        This function is an essential part of the run logic.
+        """
+        self.sf.run_sensor_flow()
+        self.sf.flatten_parsed_msg.assert_called()
+
+    def test_update_available_datapoints_called(self):
+        """
+        This function is an essential part of the run logic.
+        """
+        self.sf.run_sensor_flow()
+        self.sf.update_available_datapoints.assert_called()
+
+    def test_filter_and_publish_datapoint_values_called(self):
+        """
+        This function is an essential part of the run logic.
+        """
+        self.sf.run_sensor_flow()
+        self.sf.filter_and_publish_datapoint_values.assert_called()
