7942a09b15cd0546a8a4bfaa95dd29fa63b7292a
David Wölfle <woelfle@fzi.de>
2020-12-03 15:40:19 +0100

    Refactor dispatchers, DispatchOnce should now be functional.

--- service_templates/connectors/Python/source/pyconnector_template/dispatch.py
+++ service_templates/connectors/Python/source/pyconnector_template/dispatch.py
@@ -2,14 +2,16 @@
 # -*- coding: utf-8 -*-
 """
 """
-import threading
 import ctypes
+import inspect
+import threading
 
 
-class BaseDispatcher(threading.Thread):
+class DispatchOnce(threading.Thread):
     """
-    This is a bit hacky extension that allows terminating a running thread.
+    Execute a target function once in a thread with the abbility to cancel.
 
+    This is a bit hacky extension that allows terminating a running thread.
     Termination is done primarly by raising SystemExit. However, you should
     be aware that some commands won't receive the Exception directly,
     especially if those commands are executed as C code. This is e.g. the
@@ -26,26 +28,84 @@ class BaseDispatcher(threading.Thread):
     3.7+, see comment in terminate() for details.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(
+            self,
+            target_func=None,
+            target_args=None,
+            target_kwargs=None,
+            cleanup_func=None,
+            cleanup_args=None,
+            cleanup_kwargs=None,
+        ):
         """
-        Similar to the __init__ of Thread.
-
-        Difference is that we inject termination_event into the target
-        function an that daemon is always set to True. The later is as
-        we need to be sure that the Dispatcher will exit with the main
-        program.
-
-        See also:
-        https://docs.python.org/3/library/threading.html#threading.Thread
+        Parameters
+        ----------
+        target_func : callable, optional
+            The target that is called in the Thread. Defaults to None.
+        target_args : tuple/list, optional
+            Arguments provided to target_func. Defaults to ().
+        target_kwargs : dict, optional
+            Keywordarguments provided to target_func. Defaults to {}.
+        cleanup_func : TYPE, optional
+            A callable that is executed after the target has finished.
+            Regardeless if it exited normally or with exception.
+            Defaults to None.
+        cleanup_args : tuple/list, optional
+            Arguments provided to cleanup_func. Defaults to ().
+        cleanup_kwargs : dict, optional
+            Keywordarguments provided to cleanup_func. Defaults to {}.
         """
-        kwargs["daemon"] = True
+        # These are always daemonin, as we expect them to exit with the
+        # main program. No zombies today.
+        super().__init__(daemon=True)
 
+        if target_args is None:
+            target_args = ()
+        if target_kwargs is None:
+            target_kwargs = {}
+        if cleanup_args is None:
+            cleanup_args = ()
+        if cleanup_kwargs is None:
+            cleanup_kwargs = {}
+
+        self.target_func = target_func
+        self.target_args = target_args
+        self.target_kwargs = target_kwargs
+        self.cleanup_func = cleanup_func
+        self.cleanup_args = cleanup_args
+        self.cleanup_kwargs = cleanup_kwargs
+
+        # Create the termination event and inject it into kwargs of the
+        # target function, if the function expects it.
         self.termination_event = threading.Event()
-        if not "kwargs" in kwargs:
-            kwargs["kwargs"] = {}  # These are the kwargs of the target func.
-        kwargs["kwargs"]["termination_event"] = self.termination_event
+        if callable(self.target_func):
+            target_params = inspect.signature(self.target_func).parameters
+            if "termination_event" in target_params:
+                self.target_kwargs["termination_event"] = self.termination_event
 
-        super().__init__(*args, **kwargs)
+    def run(self):
+        """
+        Execute the target function once in thread. Call cleanup afterwards.
+
+        This executes target_func until either the function has finished
+        by itself, or until the terminate method has been called. Either
+        way we call cleanup_func and exit afterwards.
+        """
+        try:
+            if self.target_func:
+                self.target_func(*self.target_args, **self.target_kwargs)
+        except SystemExit:
+            pass
+        finally:
+            if self.cleanup_func:
+                self.cleanup_func(*self.cleanup_args, **self.cleanup_kwargs)
+            # This is taken from the Python default implementation at:
+            # https://github.com/python/cpython/blob/master/Lib/threading.py
+            #
+            # Avoid a refcycle if the thread is running a function with
+            # an argument that has a member that points to the thread.
+            del self.target_func, self.target_func, self.target_kwargs
+            del self.cleanup_func, self.cleanup_func, self.cleanup_kwargs
 
     def terminate(self):
         """
@@ -53,6 +113,7 @@ class BaseDispatcher(threading.Thread):
         SystemExit exception.
         """
         # Find the id in the list of all threads.
+        thread_id = None
         for _id, thread in threading._active.items():
             if thread is self:
                 thread_id = _id
@@ -60,36 +121,20 @@ class BaseDispatcher(threading.Thread):
 
         # Be aware of the change in Python 3.7 to PyThreadState_SetAsyncExc:
         # https://docs.python.org/3/c-api/init.html?highlight=pythreadstate_setasyncexc#c.PyThreadState_SetAsyncExc
-        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(
-            ctypes.c_ulong(thread_id),
-            ctypes.py_object(SystemExit)
-        )
+        if thread_id:
+            res = ctypes.pythonapi.PyThreadState_SetAsyncExc(
+                ctypes.c_ulong(thread_id),
+                ctypes.py_object(SystemExit)
+            )
+            # If true no thread was affected by the call above.
+            if res == 0:
+                print(
+                    "Terminating thread failed, could not find thread with "
+                    "matching id."
+                )
 
         # Fire the event after the exception has been raised, so we jump
         # to exception handling immediatly.
         self.termination_event.set()
 
-        # If not thread was affected by the call above.
-        if res == 0:
-            print(
-                "Terminating thread failed, could not find thread with "
-                "matching id."
-            )
 
-
-class DispatchInInterval(BaseDispatcher):
-
-    def __init__(self, dispatch_interval):
-        """
-        self.dispatch_interval
-
-        Parameters
-        ----------
-        dispatch_interval : TYPE
-            DESCRIPTION.
-
-        Returns
-        -------
-        None.
-
-        """
--- service_templates/connectors/Python/source/tests/test_dispatch.py
+++ service_templates/connectors/Python/source/tests/test_dispatch.py
@@ -1,38 +1,130 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from time import time
+from threading import Event
+from unittest.mock import MagicMock
+
 
 from .base import TestClassWithFixtures
-from pyconnector_template.dispatch import BaseDispatcher
+from pyconnector_template.dispatch import DispatchOnce
 
 
+class TestDispatchOnce__init__(TestClassWithFixtures):
 
-class TestBaseDispatcherTerminate(TestClassWithFixtures):
+    fixture_names = ()
+
+    def test_required_attributes_created(self):
+        """
+        Verify that all required attributes are created by __init__
+        """
+        target_func = MagicMock()
+        target_args = MagicMock()
+        target_kwargs = MagicMock()
+        cleanup_func = MagicMock()
+        cleanup_args = MagicMock()
+        cleanup_kwargs = MagicMock()
+
+        do = DispatchOnce(
+            target_func=target_func,
+            target_args=target_args,
+            target_kwargs=target_kwargs,
+            cleanup_func=cleanup_func,
+            cleanup_args=cleanup_args,
+            cleanup_kwargs=cleanup_kwargs,
+        )
+
+        assert do.target_func == target_func
+        assert do.target_args == target_args
+        assert do.target_kwargs == target_kwargs
+        assert do.cleanup_func == cleanup_func
+        assert do.cleanup_args == cleanup_args
+        assert do.cleanup_kwargs == cleanup_kwargs
+        assert isinstance(do.termination_event, Event)
+
+    def test_default_parameters(self):
+        """
+        Check that defaults for args and kwargs are created.
+        """
+        do = DispatchOnce()
+
+        assert do.target_args == ()
+        assert do.target_kwargs == {}
+        assert do.cleanup_args == ()
+        assert do.cleanup_kwargs == {}
+
+    def test_termination_event_injected_if_exected(self):
+        """
+        Verify that the termination_event is added to kwargs if the target
+        function has a termination_event argument.
+        """
+        def target_func(termination_event):
+            pass
+
+        do = DispatchOnce(target_func=target_func)
+
+        assert "termination_event" in do.target_kwargs
+        assert do.target_kwargs["termination_event"] == do.termination_event
+
+    def test_termination_event_not_injected_if_not_exected(self):
+        """
+        Verify that the termination_event is not add to kwargs if the target
+        function has no termination_event argument.
+        """
+        def target_func():
+            pass
+
+        do = DispatchOnce(target_func=target_func)
+        #do.start()
+        assert "termination_event" not in do.target_kwargs
+
+
+class TestDispatchOnceRun(TestClassWithFixtures):
+    """
+    Nothing to do here, run should work fine if the integration tests below
+    run through.
+    """
+    fixture_names = ()
+
+
+class TestDispatchOnceTerminate(TestClassWithFixtures):
+
+    fixture_names = ()
+
+    def test_termination_event_set(self):
+        """
+        Verify that the termination event is fired.
+        """
+        do = DispatchOnce()
+        do.start()
+        do.terminate()
+
+        assert do.termination_event.is_set()
+
+
+class TestDispatchOnceIntregration(TestClassWithFixtures):
+    """
+    Integration tests to verify functionality.
+    """
 
     fixture_names = ()
 
     def test_terminate_quits_thread_immediatly(self):
         """
-        The terminate method should inject a SystemExit exception into the
-        target executed by Thread.run. Ensure this happens.
+        Verify that a thread is started, and stopped immediatly after
+        terminate is called.
         """
-        got_system_exit = {"state": False}
-
-        def test_function(termination_event, got_system_exit):
+        def target_func(termination_event, got_system_exit):
             """
             This would sleep one second if the test fails and return faster
             if the SystemExit is received as expected.
             """
-            try:
-                termination_event.wait(1)
-            except SystemExit:
-                got_system_exit["state"] = True
-                raise
+            # Don't use sleep here, it will not be affected by SystemExit.
+            termination_event.wait(1)
 
-        thread = BaseDispatcher(
-            target=test_function,
-            kwargs={"got_system_exit": got_system_exit}
+        thread = DispatchOnce(
+            target_func=target_func,
         )
+
         thread.start()
         start_time = time()
 
@@ -47,8 +139,77 @@ class TestBaseDispatcherTerminate(TestClassWithFixtures):
         # Check thread has exited as expected.
         assert not thread.is_alive()
 
-        # Verify that except block has been executed.
+        # Check that the try loop has been left immediatly.
+        assert runtime < 0.1
+
+    def test_cleanup_func_called_after_terminate_called(self):
+        """
+        Verify that the cleanup function is called even if we exit
+        with SystemExit.
+        """
+
+        def target_func(termination_event):
+            """
+            This would sleep one second if the test fails and return faster
+            if the SystemExit is received as expected.
+            """
+            # Don't use sleep here, it will not be affected by SystemExit.
+            termination_event.wait(1)
+
+        def cleanup_func(got_system_exit):
+            got_system_exit["state"] = True
+
+        got_system_exit = {"state": False}
+
+        thread = DispatchOnce(
+            target_func=target_func,
+            cleanup_func=cleanup_func,
+            cleanup_kwargs={"got_system_exit": got_system_exit}
+        )
+
+        thread.start()
+        start_time = time()
+
+        # Ensure the thread was started correctly.
+        assert thread.is_alive()
+
+        # Ask the thread to exit and measure how long it takes.
+        thread.terminate()
+        thread.join()
+        runtime = time() - start_time
+
+        # Check thread has exited as expected.
+        assert not thread.is_alive()
+
+        # Verify that the cleanup function has been executed.
         assert got_system_exit["state"]
 
         # Check that the try loop has been left immediatly.
         assert runtime < 0.1
+
+    def test_cleanup_func_called_after_target_func_exits(self):
+        """
+        Verify that the cleanup function is called after the target
+        function has exited normally.
+        """
+
+        def target_func():
+            pass
+
+        def cleanup_func(got_system_exit):
+            got_system_exit["state"] = True
+
+        got_system_exit = {"state": False}
+
+        thread = DispatchOnce(
+            target_func=target_func,
+            cleanup_func=cleanup_func,
+            cleanup_kwargs={"got_system_exit": got_system_exit}
+        )
+
+        # Run thread and wait until it finished.
+        thread.start()
+        thread.join()
+
+        # Verify that the cleanup function has been executed.
+        assert got_system_exit["state"]
