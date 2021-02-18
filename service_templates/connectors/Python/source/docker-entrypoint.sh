ce719bdbb801d7e2f4a1937bc9293e54524ebfe2
David Wölfle <woelfle@fzi.de>
2021-02-18 17:05:56 +0100

    Modify entrypoint of python connector template to stop faster on docker container stop

--- service_templates/connectors/Python/source/docker-entrypoint.sh
+++ service_templates/connectors/Python/source/docker-entrypoint.sh
@@ -12,4 +12,15 @@ export MQTT_TOPIC_RAW_MESSAGE_TO_DB="${CONNECTOR_NAME}/raw_message_to_db"
 export MQTT_TOPIC_RAW_MESSAGE_REPROCESS="${CONNECTOR_NAME}/raw_message_reprocess"
 export MQTT_TOPIC_DATAPOINT_MESSAGE_WILDCARD="${CONNECTOR_NAME}/messages/#"
 
-python3 /source/connector/main.py
+python3 /source/connector/main.py &
+
+# Patches SIGTERM and SIGINT to stop the service. This is required
+# to trigger graceful shutdown if docker wants to stop the container.
+service_pid=$!
+trap "kill -TERM $service_pid" SIGTERM
+trap "kill -INT $service_pid" INT
+
+# Run until the container is stopped. Give the service maximal 2 seconds to
+# clean up and shut down, afterwards we pull the plug hard.
+wait
+sleep 2
