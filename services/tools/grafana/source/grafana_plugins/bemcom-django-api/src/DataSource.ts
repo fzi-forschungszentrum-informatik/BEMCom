caf4fb1e99f001fcbe305db0a91c8280220cc6f2
uceac <uceac@student.kit.edu>
2021-03-29 16:51:22 +0200

    grafana: working on enabling custom and grafana addQuery functionality.

--- services/tools/grafana/source/grafana_plugins/bemcom-django-api/src/DataSource.ts
+++ services/tools/grafana/source/grafana_plugins/bemcom-django-api/src/DataSource.ts
@@ -67,165 +67,167 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
     const to = range!.to.valueOf();
 
     // for queries defined by Grafana UI:
-    // options.targets.forEach((mainQuery) => {});
+    const allQueryPromises = options.targets.map((mainQuery) => {
+      // const mainQuery: MyQuery | undefined = options.targets.pop();
 
-    const mainQuery: MyQuery | undefined = options.targets.pop();
+      let targets: { [k: string]: any }[] = [];
 
-    let targets: { [k: string]: any }[] = [];
-
-    // handle getMeta
-    if (mainQuery?.getMeta) {
-      let newTarget: { [k: string]: any } = {};
-      newTarget.getMeta = mainQuery?.getMeta;
-
-      newTarget.refId = mainQuery?.refId || '';
-      newTarget.datasource = mainQuery?.datasource || '';
-      newTarget.from = from || '';
-      newTarget.to = to || '';
-
-      targets.push(newTarget);
-    } else {
-      // handle normal query if getMeta is false
-
-      mainQuery?.entries.forEach((entry) => {
+      // handle getMeta
+      if (mainQuery?.getMeta) {
         let newTarget: { [k: string]: any } = {};
-        newTarget.datapoint = entry.datapoint;
-        newTarget.datatype = entry.datatype;
-        newTarget.displayName = entry.displayName;
-        newTarget.scalingFactor = entry.scalingFactor;
+        newTarget.getMeta = mainQuery?.getMeta;
 
         newTarget.refId = mainQuery?.refId || '';
         newTarget.datasource = mainQuery?.datasource || '';
         newTarget.from = from || '';
         newTarget.to = to || '';
+
         targets.push(newTarget);
-      });
-    }
+      } else {
+        // handle normal query if getMeta is false
 
-    // const promises = options.targets.map((target) =>
-    const promises = targets.map((target) =>
-      this.doRequest(target).then((response) => {
-        if (!response || response.data === undefined || response.data.length === 0) {
-          return [];
-        }
-        if (target.getMeta) {
-          // table like data.
-          return response.data;
-        } else {
-          // time series like data.
-          var frame = new MutableDataFrame({
-            refId: target.refId,
-            name: 'timeseries',
-            fields: [{ name: 'time', type: FieldType.time }],
-          });
+        mainQuery?.entries.forEach((entry) => {
+          let newTarget: { [k: string]: any } = {};
+          newTarget.datapoint = entry.datapoint;
+          newTarget.datatype = entry.datatype;
+          newTarget.displayName = entry.displayName;
+          newTarget.scalingFactor = entry.scalingFactor;
 
-          // use displayName and scalingFactor
-          const displayName = target.displayName || target.datapoint?.label;
-          const scalingFactor = target.scalingFactor || 1;
+          newTarget.refId = mainQuery?.refId || '';
+          newTarget.datasource = mainQuery?.datasource || '';
+          newTarget.from = from || '';
+          newTarget.to = to || '';
+          targets.push(newTarget);
+        });
+      }
 
-          switch (target.datatype?.label) {
-            case 'value':
-              frame.name = 'value';
-              frame.addField({ name: displayName || 'value', type: FieldType.number });
-
-              // sort response.data by timestamps
-              response.data = response.data.sort((first: any, second: any) => {
-                return first.timestamp > second.timestamp ? 1 : -1;
-              });
-
-              response.data.forEach((point: any) => {
-                frame.appendRow([point.timestamp, point.value * scalingFactor]);
-              });
-              break;
-            case 'schedule':
-              frame.name = 'schedule';
-              frame.addField({ name: displayName || 'schedule', type: FieldType.number });
-
-              // sort response.data by timestamps
-              response.data = response.data.sort((first: any, second: any) => {
-                return first.timestamp > second.timestamp ? 1 : -1;
-              });
-
-              let latest_schedule = response.data[response.data.length - 1];
-
-              latest_schedule.schedule.forEach((interval: any) => {
-                // set frame content
-                // check if from or to is null
-                if (interval.from_timestamp == null) {
-                  frame.appendRow([interval.to_timestamp - 1, interval.value * scalingFactor]);
-                } else if (interval.to_timestamp == null) {
-                  frame.appendRow([interval.from_timestamp, interval.value * scalingFactor]);
-                } else {
-                  frame.appendRow([interval.from_timestamp, interval.value * scalingFactor]);
-                  frame.appendRow([interval.to_timestamp - 1, interval.value * scalingFactor]);
-                }
-              });
-
-              break;
-            case 'setpoint':
-              // sort response.data by timestamps
-              response.data = response.data.sort((first: any, second: any) => {
-                return first.timestamp > second.timestamp ? 1 : -1;
-              });
-
-              let latest_setpoint = response.data[response.data.length - 1];
-
-              frame.name = 'setpoint';
-              frame.addField({
-                name: displayName + '_low' || 'lower_bound',
-                type: FieldType.number,
-              });
-              frame.addField({
-                name: displayName + '_up' || 'upper_bound',
-                type: FieldType.number,
-              });
-              frame.addField({
-                name: displayName + '_pref' || 'preferred_value',
-                type: FieldType.number,
-              });
-
-              latest_setpoint.setpoint.forEach((interval: any) => {
-                let acceptable_values = interval.acceptable_values.map((x: string) => Number(x) * scalingFactor);
-                let preferred_value = Number(interval.preferred_value) * scalingFactor;
-
-                // check if from or to is null
-                if (interval.from_timestamp == null) {
-                  frame.appendRow([
-                    interval.to_timestamp - 1,
-                    Math.min(...acceptable_values),
-                    Math.max(...acceptable_values),
-                    preferred_value,
-                  ]);
-                } else if (interval.to_timestamp == null) {
-                  frame.appendRow([
-                    interval.from_timestamp,
-                    Math.min(...acceptable_values),
-                    Math.max(...acceptable_values),
-                    preferred_value,
-                  ]);
-                } else {
-                  frame.appendRow([
-                    interval.from_timestamp,
-                    Math.min(...acceptable_values),
-                    Math.max(...acceptable_values),
-                    preferred_value,
-                  ]);
-                  frame.appendRow([
-                    interval.to_timestamp - 1,
-                    Math.min(...acceptable_values),
-                    Math.max(...acceptable_values),
-                    preferred_value,
-                  ]);
-                }
-              });
-              break;
+      // const promises = options.targets.map((target) =>
+      const promises = targets.map((target) =>
+        this.doRequest(target).then((response) => {
+          if (!response || response.data === undefined || response.data.length === 0) {
+            return [];
           }
-          return frame;
-        }
-      })
-    );
+          if (target.getMeta) {
+            // table like data.
+            return response.data;
+          } else {
+            // time series like data.
+            var frame = new MutableDataFrame({
+              refId: target.refId,
+              name: 'timeseries',
+              fields: [{ name: 'time', type: FieldType.time }],
+            });
 
-    return Promise.all(promises).then((data) => ({ data }));
+            // use displayName and scalingFactor
+            const displayName = target.displayName || target.datapoint?.label;
+            const scalingFactor = target.scalingFactor || 1;
+
+            switch (target.datatype?.label) {
+              case 'value':
+                frame.name = 'value';
+                frame.addField({ name: displayName || 'value', type: FieldType.number });
+
+                // sort response.data by timestamps
+                response.data = response.data.sort((first: any, second: any) => {
+                  return first.timestamp > second.timestamp ? 1 : -1;
+                });
+
+                response.data.forEach((point: any) => {
+                  frame.appendRow([point.timestamp, point.value * scalingFactor]);
+                });
+                break;
+              case 'schedule':
+                frame.name = 'schedule';
+                frame.addField({ name: displayName || 'schedule', type: FieldType.number });
+
+                // sort response.data by timestamps
+                response.data = response.data.sort((first: any, second: any) => {
+                  return first.timestamp > second.timestamp ? 1 : -1;
+                });
+
+                let latest_schedule = response.data[response.data.length - 1];
+
+                latest_schedule.schedule.forEach((interval: any) => {
+                  // set frame content
+                  // check if from or to is null
+                  if (interval.from_timestamp == null) {
+                    frame.appendRow([interval.to_timestamp - 1, interval.value * scalingFactor]);
+                  } else if (interval.to_timestamp == null) {
+                    frame.appendRow([interval.from_timestamp, interval.value * scalingFactor]);
+                  } else {
+                    frame.appendRow([interval.from_timestamp, interval.value * scalingFactor]);
+                    frame.appendRow([interval.to_timestamp - 1, interval.value * scalingFactor]);
+                  }
+                });
+
+                break;
+              case 'setpoint':
+                // sort response.data by timestamps
+                response.data = response.data.sort((first: any, second: any) => {
+                  return first.timestamp > second.timestamp ? 1 : -1;
+                });
+
+                let latest_setpoint = response.data[response.data.length - 1];
+
+                frame.name = 'setpoint';
+                frame.addField({
+                  name: displayName + '_low' || 'lower_bound',
+                  type: FieldType.number,
+                });
+                frame.addField({
+                  name: displayName + '_up' || 'upper_bound',
+                  type: FieldType.number,
+                });
+                frame.addField({
+                  name: displayName + '_pref' || 'preferred_value',
+                  type: FieldType.number,
+                });
+
+                latest_setpoint.setpoint.forEach((interval: any) => {
+                  let acceptable_values = interval.acceptable_values.map((x: string) => Number(x) * scalingFactor);
+                  let preferred_value = Number(interval.preferred_value) * scalingFactor;
+
+                  // check if from or to is null
+                  if (interval.from_timestamp == null) {
+                    frame.appendRow([
+                      interval.to_timestamp - 1,
+                      Math.min(...acceptable_values),
+                      Math.max(...acceptable_values),
+                      preferred_value,
+                    ]);
+                  } else if (interval.to_timestamp == null) {
+                    frame.appendRow([
+                      interval.from_timestamp,
+                      Math.min(...acceptable_values),
+                      Math.max(...acceptable_values),
+                      preferred_value,
+                    ]);
+                  } else {
+                    frame.appendRow([
+                      interval.from_timestamp,
+                      Math.min(...acceptable_values),
+                      Math.max(...acceptable_values),
+                      preferred_value,
+                    ]);
+                    frame.appendRow([
+                      interval.to_timestamp - 1,
+                      Math.min(...acceptable_values),
+                      Math.max(...acceptable_values),
+                      preferred_value,
+                    ]);
+                  }
+                });
+                break;
+            }
+            return frame;
+          }
+        })
+      );
+
+      return Promise.all(promises).then((data) => ({ data })); // TODO: find out how to parse promises and data correctly
+    });
+
+    return Promise.all(allQueryPromises).then((data) => ({ data }));
   }
 
   async testDatasource() {
