18036c1065db4fa4085162d16ae813322d7685d2
David Wölfle <woelfle@fzi.de>
2021-04-20 16:19:06 +0200

    Python Connector Template: Remove obsolte parts from entrypoint script.

--- service_templates/connectors/Python/source/setup.py
+++ service_templates/connectors/Python/source/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pyconnector_template",
-    version="0.1.2",
+    version="0.2.1",
     install_requires=["python-dotenv==0.15.*", "paho-mqtt==1.5.*", "pytest"],
     packages=setuptools.find_packages(),
 )
