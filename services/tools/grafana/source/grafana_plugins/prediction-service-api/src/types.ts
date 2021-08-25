0c21ad3ec9b8d57779afcb55a348191e4b668d62
uceac <uceac@student.kit.edu>
2021-08-25 12:24:48 +0200

    grafana-tool: copied BEMCOM Grafanaplugin to start new plugin for prediction services

---
+++ services/tools/grafana/source/grafana_plugins/prediction-service-api/src/types.ts
@@ -0,0 +1,64 @@
+import { DataQuery, DataSourceJsonData } from '@grafana/data';
+
+export interface MyQuery extends DataQuery {
+  getMeta: boolean;
+
+  datapoint: MyDatapoint;
+  datatype: MyDatatype;
+  displayName: string;
+  scalingFactor: number;
+
+  interval?: string;
+  offset: string;
+  useIntervalAndOffset?: boolean;
+
+  nQueries: number;
+  from?: number;
+  to?: number;
+}
+
+// defaults:
+export const defaultQuery: Partial<MyQuery> = {
+  getMeta: false,
+  datapoint: { label: '', value: 0, description: '' },
+  datatype: { label: 'value', value: 0, description: 'timeseries of values' },
+  displayName: '',
+  scalingFactor: 1,
+  useIntervalAndOffset: true,
+};
+
+/**
+ * These are options configured for each DataSource instance
+ */
+export interface MyDataSourceOptions extends DataSourceJsonData {
+  useBasicAuth?: boolean;
+  basicAuthUser?: string;
+  tlsSkipVerify?: boolean;
+  queryLimit?: number;
+
+  user?: string;
+}
+
+// export const defaultDataSourceOptions: Partial<MyDataSourceOptions> = {
+//   useBasicAuth: false,
+// };
+
+/**
+ * Value that is used in the backend, but never sent over HTTP to the frontend
+ */
+export interface MySecureJsonData {
+  basicAuthPassword?: string;
+}
+
+// custom data types
+export interface MyDatapoint {
+  label: string;
+  value: number;
+  description: string;
+}
+
+export interface MyDatatype {
+  label: string;
+  value: number;
+  description: string;
+}
