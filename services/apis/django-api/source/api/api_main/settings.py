db249d6f9518a2436c6714779c25a7b3e6f307c7
David Wölfle <woelfle@fzi.de>
2022-02-21 16:22:45 +0100

    django-api: Update django to 3.2 and all other requirements to the latest version.

--- services/apis/django-api/source/api/api_main/settings.py
+++ services/apis/django-api/source/api/api_main/settings.py
@@ -256,5 +256,5 @@ SPECTACULAR_SETTINGS = {
         "BEMCom/blob/master/documentation/Readme.md) for more information."
     ),
     "LICENSE": {"name": "Licensed under MIT"},
-    "VERSION": "0.8.0",
+    "VERSION": "0.8.1",
 }
