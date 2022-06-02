363f2c7be692ba1795f7e63f30a396a69417a099
David Wölfle <woelfle@fzi.de>
2022-06-02 21:15:31 +0200

    emp-link-tool: Restructure docker image to allow building for ARM-7 too.

--- services/tools/emp-link/source/entrypoint.sh
+++ services/tools/emp-link/source/entrypoint.sh
@@ -5,11 +5,11 @@ set -u
 echo "Entering entrypoint.sh"
 if [ "${AUTO_RELOAD:-FALSE}" == "TRUE" ]
 then
-    /opt/conda/bin/watchmedo auto-restart \
+    watchmedo auto-restart \
       --directory=/source/emp-link/ \
       --patterns="*.py" \
       --recursive \
-      -- /opt/conda/bin/python3 /source/emp-link/main.py
+      -- python3 /source/emp-link/main.py
 else
-    /opt/conda/bin/python3 /source/emp-link/main.py
+    python3 /source/emp-link/main.py
 fi
