ab7b698116c92af0ebef3230f7bd5f7e38acda00
David Wölfle <woelfle@fzi.de>
2025-05-07 10:07:59 +0200

    mosquitto-debian-mqtt-broker: Add initial version of alternative build that supports ARM too.

---
+++ services/mqtt-brokers/mosquitto-debian-mqtt-broker/Readme.md
@@ -0,0 +1,11 @@
+# Debian Mosquitto MQTT Broker
+
+MQTT message broker service based on the [Debian docker image](https://hub.docker.com/_/debian), see https://hub.docker.com/_/eclipse-mosquitto for details. This is provided here as an alternative to the official [Mosquitte image](https://hub.docker.com/_/eclipse-mosquitto) as the latter does not provide a ARMv7 build.
+
+
+
+### Changelog
+
+| Tag   | Changes                                                      |
+| ----- | ------------------------------------------------------------ |
+| 0.1.0 | Initial version, uses Mosquitto 2.0.11 (latest stable as of 2025-05-06) |
---
+++ services/mqtt-brokers/mosquitto-debian-mqtt-broker/build_and_push_docker_image.sh
@@ -0,0 +1,17 @@
+# This script assumes that docker is configured with a builder that can 
+# build multi-platform images. These commands may help you create such:
+# docker buildx create --name bemcom_builder --use
+# docker buildx inspect --bootstrap
+# See also:
+# https://docs.docker.com/build/building/multi-platform/
+VERSION=0.1.0
+printf "\033[0;31m" # make red
+read -p "Building and pushing version $VERSION. Are you sure? (y/n) " yn
+printf "\033[0m" # no color.
+case $yn in 
+	y ) echo Starting build.;;
+	* ) echo Exiting.;
+		exit;;
+esac
+
+docker buildx build --push --platform linux/arm/v7,linux/amd64 ./source -t bemcom/mosquitto-debian-mqtt-broker:$VERSION
